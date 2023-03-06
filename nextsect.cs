37f0baf81ddb048b816303b002a0304ce3aa285c
potatogames <111431356+pottogames@users.noreply.github.com>
2023-03-06 11:46:59 +0200

    Add files via upload

---
+++ nextsect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nextsect : MonoBehaviour
+{
+    
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Level_Menu");
+    }
+}
+
+void Update()
+    {
+        
+    }
+}
